b5003e665f59754dd0e70e8e5a2c04adf53dbe9a
PAVIPREM <144780486+PAVIPREM@users.noreply.github.com>
2023-09-12 13:43:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# leap year
+
+"""
+year % 4 == 0
+year % 100 != 0 /
+year % 400 == 0
+
+"""
+def isLeapyear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+    return False
+
+year= 2013
+
+if isLeapyear(year):
+  print('{}is a Leap year.'.format(year))
+else:
+  print('{} is not a Leap year .'.format(year))
+  
